8abe19393218b3a285d1a8f3e3069322beba89b4
abeishbabu <abeishbabu@gmail.com>
2016-10-15 20:06:14 +0530

    modified index file.

--- angularjs-II/module-1/scripts/app.js
+++ angularjs-II/module-1/scripts/app.js
@@ -11,16 +11,33 @@ function LunchCheckController($scope){
   $scope.checkLunch = function(){
     var lunch = $scope.lunch || ""
     var items = lunch.split(",");
-    var cnt = items.length;
+    var cnt = 0;
 
-    if (lunch === "")
+    for ( var i = 0; i < items.length; i = i + 1){  // elimiate empty value
+      if (items[i] != "")
+        cnt = cnt + 1
+    }
+
+    if (lunch === ""){
       $scope.msg = "Please enter data first";
-    else if (cnt >0 && cnt <= 3)
+      $scope.msgclass= "message-error";
+      $scope.borderclass= "border-error";
+    }
+    else if (cnt >0 && cnt <= 3){
       $scope.msg = "Enjoy!";
-    else if (cnt > 3)
-      $scope.msg = "Too Much";
-    else
+      $scope.msgclass= "message-success";
+      $scope.borderclass= "border-success";
+    }
+    else if (cnt > 3){
+      $scope.msg = "Too Much!";
+      $scope.msgclass= "message-success";
+      $scope.borderclass= "border-success";
+    }
+    else{
       $scope.msg = "Please enter data first";
+      $scope.msgclass= "message-error";
+      $scope.borderclass= "border-error";
+    }
 
     console.log(lunch + cnt);
   };
