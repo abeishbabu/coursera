6a68af735ae8a0f51581dbb0fe06a82e074fe1d8
abeishbabu <abeishbabu@gmail.com>
2016-11-17 03:44:14 +0530

    Update menudata.service.js

--- angularjs-II/module-4/src/menudata.service.js
+++ angularjs-II/module-4/src/menudata.service.js
@@ -4,6 +4,22 @@ angular.module('data')
  
  function MenuDataService (){
     console.log("inside MenuDataService");
+    var service = this;
+    
+    service.getAllCategories = function(){
+      var response = $http({
+			       method: "GET",
+			       url: "https://davids-restaurant.herokuapp.com/categories.json"
+		    });
+      return response;
+    };
+  
+   service.getItemsForCategory = function(categoryShortName){
+     var response = $http({
+         method: "GET",
+         url: "https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName
+     });
+   };
  }
 
 
