1715bac490b6094756be67263a09a3a81a7f9dc7
abeishbabu <abeishbabu@gmail.com>
2016-11-19 04:55:12 +0530

    Update menudata.service.js

--- angularjs-II/module-4/src/menudata.service.js
+++ angularjs-II/module-4/src/menudata.service.js
@@ -9,13 +9,7 @@ angular.module('data')
     var service = this;
     
     service.getAllCategories = function(){
-	/*console.log("ïnside getAllCategories");
-      var response = $http({
-		method: "GET",
-		url: baseAPIURL +  "categories.json"
-      });
-
-      return response;*/
+	
       	return $http.get(baseAPIURL + '/categories.json').then(function (response) {
       		return response.data;
 	});
@@ -23,13 +17,7 @@ angular.module('data')
 
 
     service.getItemsForCategory = function(categoryShortName){
-	/*console.log("ïnside getItemsForCategory for:" + categoryShortName);
-      var response = $http({
-		method: "GET",
-		url: baseAPIURL +  "menu_items.json?category=" +  categoryShortName
-      });
-
-      return response;*/
+	
 	var config = {};
     	if (categoryShortName) {
       		config.params = {category: categoryShortName};
